f6a7078e51af28ed78fb4d96cdb6872495eb1478
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-08 12:51:17 +0200

    "Q2"

--- quiz1.py
+++ quiz1.py
@@ -1,2 +1,14 @@
 output = len('95637+12')
 print(f'the ouput of the code is : {output}')
+
+
+
+score = 67 
+if score < 80 and score > 70:
+    print("A")
+elif score < 90 or score > 80:
+    print("B")
+elif score > 60 : 
+    print("C")
+else: 
+    print("D")
