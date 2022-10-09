dd21a76a4b771dfbd6a16dc2bc95f0e81456ebf2
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-09 22:54:10 +0200

    This is day_1 Project

---
+++ Day1_BandName_Generator.py
@@ -0,0 +1,5 @@
+#Day1 Project ---> BandName Generator
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pets_name = input("What's your pet's name?\n")
+print('Your band name is ' + city + " " + pets_name)
