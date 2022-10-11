7f4b0d5949be2bf28f610ede42437db1e611aa56
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-11 17:18:35 +0200

    "Tip calculator version 2.0"

--- Day2_Tip_Calculator.py
+++ Day2_Tip_Calculator.py
@@ -1,5 +1,5 @@
-print("Welcome to the tip calculator")
-total_bill = float(input("What is the total bill? "))
+print("Welcome to the tip calculator!")
+total_bill = float(input("What is the total bill in USDollars? $"))
 percent_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 percent_tip = (total_bill/100 ) * percent_tip
 final_bill = total_bill + percent_tip
