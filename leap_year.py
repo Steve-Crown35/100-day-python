f74b07dc921b2fb5f6b9dd764f7cbc536d5e8729
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-25 22:13:37 +0200

    leap_year v1.2

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,7 @@
 year = int(input("Enter a year\n"))
 if year % 4 == 0:
     if year % 100 == 0:
-        if year % 400:
+        if year % 400 == 0:
             print(f"{year} is a leap year")
         else:
             print(f"{year} is not a leap year")
