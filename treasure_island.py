355e32e4effb4846c911ec2a90b2b17ee09fbdb0
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-28 21:22:10 +0200

    update 1

--- treasure_island.py
+++ treasure_island.py
@@ -1,8 +1,8 @@
-print("Welcome to the Treasure Island. Your mission is to find the hidden treasure that will make you the richest person in the world")
+print("Welcome to the Treasure Island. Your mission is to find the hidden treasure that will make you the richest person in the world.")
 first_direction = input("Choose your direction. East, West or South\n")
 first_direction = first_direction.lower()
 if first_direction == "east":
-    second_direction = input("Choose right or left")
+    second_direction = input("Choose right or left\n")
     second_direction = second_direction.lower()
     if second_direction == "right":
         print("Wrong direction. Game Over!")
@@ -12,7 +12,7 @@ if first_direction == "east":
         if get_to_island == "swim":
             print("You have been terminated by a shark. Game over!")
         elif get_to_island == "wait for a boat":
-            pick_a_color = input("choose a color. Red, Blue or Green")
+            pick_a_color = input("choose a color. Red, Blue or Green\n")
             pick_a_color = pick_a_color.lower()
             if pick_a_color == "green":
                 print("Big Congratulations. The treasure is yours. You won.")
@@ -23,6 +23,7 @@ if first_direction == "east":
     else:
             print("You entered the wrong expression. Game over!")
 elif first_direction == "south":
+    print("This direction has a longer route to the treasure island but you can still get there if you do not give up along the way.")
     movement = input("How would you like to proceed? walk or run\n")
     movement = movement.lower()
     if movement == "walk":
@@ -33,12 +34,12 @@ elif first_direction == "south":
         if food_choice == "rice":
             print("Not the right meal to eat for a walk across the desert. Game over!")
         elif food_choice == "beans":
-            direction = input("Pick a direction. Nortth or South\n")
+            direction = input("Pick a direction. North or South\n")
             direction = direction.lower()
             if direction == "north":
                 print("No treasure in this direction. You lost")
             elif direction == "south":           
-                pick_a_color = input("choose a color. Red, Blue or Green")
+                pick_a_color = input("choose a color. Red, Blue or Green\n")
                 pick_a_color = pick_a_color.lower()
                 if pick_a_color == "green":
                     print("Big Congratulations. The treasure is yours. You won.")
