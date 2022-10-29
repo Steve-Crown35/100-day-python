f56185d904a57c420c60b639bd8b5c05e308d330
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-10-29 21:30:16 +0200

    This program is not working as expected

---
+++ bank_roulette.py
@@ -0,0 +1,16 @@
+import random
+#test_seed = int(input("create a seed number: "))
+#random.seed(test_seed)
+
+names = input("Give me the list of names with each name seperated by a comma\n")
+print(names)
+#split the names using a comma as a seperator
+names.split(',' , len(names))
+print(names)
+number_of_names = len(names)
+print(number_of_names)
+random_index = random.randint(0, number_of_names-1)
+random_name = names[random_index]
+print(random_name)
+
+#print(f"{random_name} is going to buy the meal today!")'''
