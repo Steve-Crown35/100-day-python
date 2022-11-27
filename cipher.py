bef5f29f5a214d20b3564dfe2ceb9eac648d80b0
Stephen Asuquo Okon <stephenasuquo29@gmail.com>
2022-11-27 19:14:08 +0100

    updated

--- cipher.py
+++ cipher.py
@@ -1,13 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def cipher(cipher_direction, start_text, shift_amount):
     end_text = ""
-    for i in start_text: 
+    for i in start_text:
+        
         if direction == "encode":
             if i == " ":
                 new_letter = "*"
@@ -18,8 +19,8 @@ def cipher(cipher_direction, start_text, shift_amount):
             elif i == ",":
                 new_letter = "`"
                 end_text += new_letter
-            else:
-                position = alphabet.index(i)
+            else: 
+                position = alphabet.index(i)                
                 new_position = position + shift
                 new_letter = alphabet[new_position]
                 end_text += new_letter
@@ -34,13 +35,13 @@ def cipher(cipher_direction, start_text, shift_amount):
                 new_letter = ","
                 end_text += new_letter
             else:
-                position = alphabet.index(i)
+                position = alphabet.index(i) 
                 new_position = position - shift
                 new_letter = alphabet[new_position]
                 end_text += new_letter  
 
         
-    print(f"the {direction}d message is : {end_text}")
+    print(f"the {cipher_direction}d message is : {end_text}")
 
 
     
